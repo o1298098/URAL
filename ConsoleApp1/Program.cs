5002f6592d7dd3e068e14f46af5ae9dfd26791e8
hzb <o1298098@live.com>
2019-01-15 08:13:45 +0800

    无

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,71 +9,116 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static int[] x;
-        static int[] y;
-        static int[] y2;
-        static int[,] type;
-        static int n;
+        static int n, m, count, ans;
+        static int[,,] p;
+        static int[,,] type = new int[4, 12, 3] { 
+            { { 1, 0, 1 }, { 1, 0, 3 }, { 0, -1, 1 }, { 0, -1, 3 }, { 1, -1, 0 }, { 1, -1, 2 }, { 0, 1, 1 }, { 0, 1, 3 }, { -1, 0, 1 }, { -1, 0, 3 }, { -1, 1, 0 }, { -1, 1, 2 } }, 
+            { { -1, -1, 1 }, { -1, -1, 3 }, { -1, 0, 0 }, { -1, 0, 2 }, { 0, -1, 0 }, { 0, -1, 2 }, { 1, 1, 1 }, { 1, 1, 3 }, { 1, 0, 0 }, { 1, 0, 2 }, { 0, 1, 0 }, { 0, 1, 2 } }, 
+            { { 1, 0, 3 }, { 1, 0, 1 },{ 0, -1, 3 }, { 0, -1, 1 }, { 1, -1, 2 }, { 1, -1, 0 }, { 0, 1, 3 }, { 0, 1, 1 }, { -1, 0, 3 }, { -1, 0, 1 }, { -1, 1, 2 }, { -1, 1, 0 }}, 
+            { { -1, -1, 3 }, { -1, -1, 1 }, { -1, 0, 2 }, { -1, 0, 0 }, { 0, -1, 2 }, { 0, -1, 0 }, { 1, 1, 3 }, { 1, 1,1 }, { 1, 0, 2 }, { 1, 0, 0 }, { 0, 1, 2 }, { 0, 1,0 } } };
+        static int[] rd = {2,3,0,1 };
         static void Main(string[] args)
         {
             while (true)
             {
-                n = int.Parse(Console.ReadLine());
-                x = new int[n];
-                y = new int[n];
-                y2 = new int[n];
-                int count = 0;
-                for (int i = 0; i < n; i++)
+                string[] str = Console.ReadLine().Split(' ');
+                n = int.Parse(str[0]);
+                m = int.Parse(str[1]);
+                p = new int[n, m, 4];
+                ans = 0;
+                GetInput(1);
+                GetInput(2);
+                while (count > 0)
                 {
-                    string[] s = Console.ReadLine().Split(' ');
-                    x[i] = int.Parse(s[0]);
-                    y[i] = int.Parse(s[1]);
-                }
-                for (int i = 0; i < n; i++)
-                    for (int j = i + 1; j < n; j++)
-                    {
-                        if (j + 1 > n - 1)
-                            break;
-                        int r = j - i;
-                        y2 = (int[])y.Clone();
-                        for (int k = j + 1; k < n; k++)
-                        {
-                            y2 = (int[])y.Clone();
-                            int r2 = k - j;
-                            int r3 = k - i;
-                            type = new int[2, 3] {{ r, r2, -r3 }, { r3, -r, -r2 }};
-                            for (int l = 0; l < type.GetLength(0); l++)
+                    for (int i = 0; i < n; i++)
+                        for (int j = 0; j < m; j++)
+                            for (int k = 0; k < 4; k++)
                             {
-                                y2[i] = y[i + type[l, 0]];
-                                y2[j] = y[j + type[l, 1]];
-                                y2[k] = y[k + type[l, 2]];
-                                if (check())
-                                    count++;
-                            }
-                        }
+                                if (p[i, j, k] == 1)
+                                {
+                                    count--;
+                                    ans++;
+                                    p[i, j, k] = 0;
+                                    dfs(i, j, k);
+                                }
 
-                    }
-                Console.WriteLine(count);
+                            }
+                }
+                Console.WriteLine(ans);
                 //break;
             }
         }
 
-        static bool check()
+        private static void dfs(int i, int j, int k)
         {
-            for (int i = 0; i< n; i++)
-            {
-                for (int j = i + 1; j < n; j++)
+            if (count <= 0)
+                return;
+            bool x = false;
+            bool y = false;
+            for (int l = 0; l < 6; l++)
+                if (i + type[k, l, 0] <= n - 1&&i + type[k, l, 0] >=0 && j + type[k, l, 1] <= m - 1&& j + type[k, l, 1]>=0)
+                    if (p[i + type[k, l, 0], j + type[k, l, 1], type[k, l, 2]] == 1)
+                    {
+                        count--;
+                        x = true;
+                        p[i + type[k, l, 0], j + type[k, l, 1], type[k, l, 2]] = 0;
+                        dfs(i + type[k, l, 0], j + type[k, l, 1], type[k, l, 2]);
+                        break;
+                    }
+            for (int l = 6; l < 12; l++)
+                if (i + type[k, l, 0] <= n - 1 && i + type[k, l, 0] >= 0 && j + type[k, l, 1] <= m - 1 && j + type[k, l, 1] >= 0)
+                    if (p[i + type[k, l, 0], j + type[k, l, 1], type[k, l, 2]] == 1)
+                    {
+                        count--;
+                        y = true;
+                        p[i + type[k, l, 0], j + type[k, l, 1], type[k, l, 2]] = 0;
+                        dfs(i + type[k, l, 0], j + type[k, l, 1], type[k, l, 2]);
+                        break;
+                    }
+            if ((!x || !y) && (i == 0 || i == n - 1 || j == 0 || j == m - 1))
+                if (p[i, j, rd[k]] == 1)
                 {
-                    int sx = Math.Abs(x[i] - x[j]);
-                    int sy = Math.Abs(y2[i] - y2[j]);
-                    if (sx == sy)
-                        return false;
+                    count--;
+                    p[i, j, rd[k]] = 0;
+                    dfs(i, j, rd[k]);
                 }
-             }
-            return true;
         }
 
-
+        private static void GetInput(int time)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    char c = (char)Console.Read();
+                    switch (c)
+                    {
+                        case '/':
+                            if (time == 1)
+                                p[i, j, 0] = 1;
+                            else
+                                p[i, j, 2] = 1;
+                            count++;
+                            break;
+                        case '\\':
+                            if (time == 1)
+                                p[i, j, 1] = 1;
+                            else
+                                p[i, j, 3] = 1;
+                            count++;
+                            break;
+                        case 'X':
+                            if (time == 1)
+                                p[i, j, 0] = p[i, j, 1] = 1;
+                            else
+                                p[i, j, 2] = p[i, j, 3] = 1;
+                            count += 2;
+                            break;
+                    }
+                }
+                Console.ReadLine();
+            }
+        }
     }
 }
 
